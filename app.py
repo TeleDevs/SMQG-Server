490c51184a7650cb46790d7aca4d91e92b6d0950
gu054642 <guilherme.roque@intelbras.com.br>
2021-04-21 16:49:17 -0300

    update

--- app.py
+++ app.py
@@ -7,66 +7,60 @@ from sqlalchemy.ext.automap import automap_base
 from sqlalchemy.orm import sessionmaker
 import json
 
-#DB credentials
+# DB credentials
 user_mysql = sys.argv[1]
 pass_mysql = sys.argv[2]
 model = sys.argv[3]
-create = sys.argv[4]
 
-#DB access
+# DB access
 engine = create_engine("mysql+mysqlconnector://" + user_mysql + ":" + pass_mysql + "@localhost:3306/" + model)
 Session = sessionmaker(bind=engine)
 Base = automap_base()
 Base.prepare(engine, reflect=True)
 
-#DB tables
-Gateway = Base.classes.Gateway
+# DB tables
+Local = Base.classes.Local
 Sensor = Base.classes.Sensor
 Reading = Base.classes.Reading
 
-#MQTT definitions
+# MQTT definitions
 client_name = socket.gethostname()
 client = mqtt.Client(client_name)
-broker = "localhost"
+broker = sys.argv[4]
 topic = sys.argv[5]
 
-#mosquitto_pub -m '{"idGateway":1,"idSensor":1,"val":23,"datetime":"2021-03-14 13:07:08.104114"}' -t 'SMQG'
+
+# mosquitto_pub -h mqtt.sj.ifsc.edu.br -m '{"idLocal":"local1","idSensor":"saca1","temperature":23,"humidity":25,"datetime":"2021-03-14 13:07"}' -t 'Empresa1'
 def on_message(client, userdata, message):
     message_rcv = str(message.payload.decode("utf-8"))
     print("message received ", message_rcv)
-    # print("message topic=", message.topic)
-    # print("message qos=", message.qos)
-    # print("message retain flag=", message.retain)
-
     message_json = json.loads(message_rcv)
     reading = Reading()
     dump_json(reading, message_json)
-    print(reading.idGateway)
-    print(reading.idSensor)
-    print(reading.val)
-    print(reading.col)
+    gateway = sessionSQL.query(Local).filter(Local.idLocal == reading.idLocal).first()
+    if gateway is None:
+        gateway = Local()
+        gateway.idLocal = reading.idLocal
+        sessionSQL.add(gateway)
+
+    sensor = sessionSQL.query(Sensor)\
+        .filter(Sensor.idSensor == reading.idSensor, Sensor.idLocal == reading.idLocal).first()
+    if sensor is None:
+        sensor = Sensor()
+        sensor.idSensor = reading.idSensor
+        sensor.idLocal = reading.idLocal
+        sessionSQL.add(sensor)
+
     sessionSQL.add(reading)
     sessionSQL.commit()
 
 
 def dump_json(reading, message_json):
-    reading.idGateway = message_json["idGateway"]
+    reading.idLocal = message_json["idLocal"]
     reading.idSensor = message_json["idSensor"]
-    reading.val = message_json["val"]
+    reading.temperature = message_json["temperature"]
+    reading.humidity = message_json["humidity"]
     reading.col = message_json["datetime"]
-
-
-def create_db():
-    gateway = Gateway()
-    gateway.idGateway = 1
-    sensor = Sensor()
-    sensor.idGateway = 1
-    sensor.idSensor = 1
-    sensor.type = "DHT11"
-    sessionSQL.add(gateway)
-    sessionSQL.add(sensor)
-    sessionSQL.commit()
-    sessionSQL.close()
 
 
 def exit_handler(sig=None, frame=None):
@@ -83,13 +77,11 @@ def start_client():
     client.loop_forever()
 
 
-#Run syntax:
-#python3 app.py <mysql-user> <mysql-pass> <mysql-model> <create-stub-gateway&sensor> <mqtt-topic>
-#Ex:
-#python3 app.py guilherme password smqg False SMQG
+# Run syntax:
+# python3 app.py <mysql-user> <mysql-pass> <mysql-model> <mqtt-broker> <mqtt-topic>
+# Ex:
+# python3 app.py guilherme password smqg SMQG
 if __name__ == '__main__':
-    if create == 'True':
-        create_db()
     sessionSQL = Session()
     signal.signal(signal.SIGINT, exit_handler)
     start_client()
